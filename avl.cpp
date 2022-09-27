613c0fc2208a6d9e5b00dc59058ae6a2be81dcff
tarunkumar242000 <110759150+tarunkumar242000@users.noreply.github.com>
2022-09-27 17:03:53 +0530

    Add files via upload

--- avl.cpp
+++ avl.cpp
@@ -49,6 +49,14 @@ void inorder(Node* h)
         inorder(h->right);
 }
 
+ void preorder(Node* jj)
+    {
+        cout<<jj->val<<" ";
+        if(jj->left)
+            preorder(jj->left);
+        if(jj->right)
+            preorder(jj->right);
+    }
 
 class bst
 {
@@ -126,11 +134,13 @@ class bst
         // inorder(head);
         // cout<<endl;
         //cout<<tail->val<<" ";
+       
         return tail;
 
      }   
         
 };
+
 
 
 
@@ -224,7 +234,7 @@ class AVL
                 Node* aa=target->right;
                 Node* bb=aa->left;
             
-                if(target->val >= teempo->val)
+                if(target->val > teempo->val)
                 {
                    
                     aa->left=target;
@@ -322,7 +332,7 @@ class AVL
     
                 aa->left=b1;
     
-                if(target->val >= teempo->val)
+                if(target->val > teempo->val)
                 {
                    
                     bb->left=target;
@@ -377,8 +387,10 @@ class AVL
                     bb->left=target;
                 
                     target->right=a;
-        
-                    teempo->left=bb;
+                    if(teempo!=target)
+                        teempo->left=bb;
+                    else
+                        b.head=bb;
                      int cd=0;
                     int zx=0;
                     int gh=0;
@@ -437,9 +449,9 @@ class AVL
                     target->left=bb;
                     
                     if(teempo!=target)
-                        teempo->right=bb;
+                        teempo->right=aa;
                     else
-                        b.head=bb;
+                        b.head=aa;
                     int cd=0;
                     int zx=0;
                     int gh=0;
@@ -472,8 +484,10 @@ class AVL
                     aa->right=target;
                   
                     target->left=bb;
-                
-                    teempo->left=bb;
+                    if(target!=teempo)
+                        teempo->left=aa;
+                    else
+                        b.head=aa;
                     int cd=0;
                     int zx=0;
                     int gh=0;
@@ -572,8 +586,10 @@ class AVL
                     bb->right=target;
                     
                     target->left=a;
-                
-                   teempo->left=bb;
+                    if(teempo!=target)
+                        teempo->left=bb;
+                    else
+                        b.head=bb;
                    int cd=0;
                     int zx=0;
                     int gh=0;
@@ -619,16 +635,18 @@ class AVL
 
     void dlt(int value)
     {
+       
+        int ct=count_occurence(value);
+        
+       while(ct--) {
         Node* temp;
         Node* temp1;
         temp1=b.head;
-        cout<<1<<endl;
         if(!temp1)
             return;
         while(temp1){                   //finding whether the required node exist 
         if(temp1->val == value)
         {
-            cout<<2<<endl;
             temp=temp1;
             break;
         }
@@ -636,12 +654,12 @@ class AVL
         {
             if(temp1->left)
             {
-                cout<<3<<endl;
+
                 temp1=temp1->left;
             }
             else
             {
-                cout<<4<<endl;
+
                 return;
             }
         } 
@@ -649,7 +667,7 @@ class AVL
         {
             if(temp1->right)
             {
-                cout<<5<<endl;
+    
                 temp1=temp1->right;
             }
             else
@@ -659,14 +677,14 @@ class AVL
             }
         }
         }
-        cout<<7<<endl;
+        
         int right=0;
         int left=0;
         
 
         if(temp->left)                         //finding inorder predecessor or successor
         {
-            cout<<8<<endl;
+        
             left=1;
             temp=temp->left;
             if(temp->right)
@@ -683,21 +701,23 @@ class AVL
         }
         else if(temp->right)
         {
-            cout<<11<<endl;
+    
             right=1;
             temp=temp->right;
             if(temp->left)
             {
-                cout<<12<<endl;
                 temp=temp->left;
                 while(temp->left)
                 {
-                    cout<<13<<endl;
                     temp=temp->left;
                 }
             }
         }
-        temp1->val=temp->val;
+    
+        int p;
+        p=temp->val;
+        int casu=0;
+       // temp1->val=temp->val;
          if(temp1 == temp)                        //deleting the node from bst
         {
             cout<<14<<endl;
@@ -705,36 +725,46 @@ class AVL
             {
                 if(temp1->prev->val > temp->val)
                 {
-                    cout<<15<<endl;
+                    casu=1;
+                    
                      temp1->prev->left=NULL;
                      int a=0;
                      if(temp->prev->right)
                         a=temp->prev->right->height;
                      temp1->prev->height=a+1;
-                    
+                     temp=temp->prev;
+                     temp1->prev=NULL;
+                     temp1=temp;
                 }
+               
+                    
+                
             else{
-                cout<<16<<endl;
+                
+                casu=1;
                 temp1->prev->right=NULL;
                 int a=0;
                 if(temp->prev->left)
                     a=temp->prev->left->height;
                 temp1->prev->height=a+1;
-                cout<<17<<endl;
+                temp=temp->prev;
+                temp1=temp;
+               }
                 }
-        }
+        
         else
         {
             cout<<18<<endl;
             b.head=NULL;
             return;
         }
+    
         }
         else
         {
             cout<<19<<endl;
             cout<<"temp"<<temp->val<<endl;
-            temp1->val=temp->val;
+          //  temp1->val=temp->val;
             if(temp->prev)
             {if(temp->prev->val > temp->val)
                { temp->prev->left=NULL;
@@ -806,16 +836,20 @@ class AVL
                     cout<<28<<endl;
                     temp->height=maxx(left,right)+1;
                 }
+            if(casu==0)
+                temp1->val=p;
 
-            
             Node* taail=temp1;
+            cout<<"taail->val"<<taail->val<<endl;
             int arr[2];
 
      while(taail){
             while(taail)                 //balance factor from target to root 
         {
             cout<<29<<endl;
-            
+            cout<<"inorder";
+            preorder(b.head);
+            cout<<endl;
             int left=0;
             int right=0;
             
@@ -864,6 +898,8 @@ class AVL
             for(int i=0;i<2;i++)
             {
                 int d=0;
+                int left=0;
+                int right=0;
                 
                 if(taail->left){
                     cout<<35<<endl;
@@ -886,6 +922,7 @@ class AVL
                 cout<<38<<endl;
                 arr[i]=1;
                 taail=taail->left;
+
             }
             else 
             {
@@ -895,7 +932,9 @@ class AVL
             }
             }
         }
-        Node* teempo,*target;
+        cout<<39<<endl;
+        Node* teempo;
+        Node* target;
         target=tarun;
         teempo=target;
         if(target->prev)
@@ -1147,12 +1186,12 @@ class AVL
                     
                     if(teempo!=target)
                         {
-                            teempo->right=bb;
+                            teempo->right=aa;
                             taail=teempo;
                         }
                     else
                         {
-                            b.head=bb;
+                            b.head=aa;
                             taail=NULL;
                         }
                     int cd=0;
@@ -1188,7 +1227,7 @@ class AVL
                   
                     target->left=bb;
 
-                    teempo->left=bb;
+                    teempo->left=aa;
                     taail=teempo;
                     int cd=0;
                     int zx=0;
@@ -1341,10 +1380,368 @@ class AVL
 
         
         }
-     }
         
      }
+       }
+         
+     }
+    bool search(int value)
+    {
+        Node* temp1;
+        temp1=b.head;
+        if(!temp1)
+            return false;
+        while(temp1)
+        {                                   //finding whether the required node exist 
+        if(temp1->val == value)
+        {
+              return true;         
+        }
+        else if(temp1->val > value) 
+        {
+            
+            
+                
+                temp1=temp1->left;
+            
+            
+        } 
+        else
+        {
+            
+                temp1=temp1->right;
+            
+            
+        }
+        }
+        return false;
+    }
 
+    int count_occurence(int value)
+    {
+         Node* temp1;
+        temp1=b.head;
+        if(!temp1)
+            return 0;
+        while(temp1)
+        {                                   
+        if(temp1->val == value)
+        {
+              return temp1->count;         
+        }
+        else if(temp1->val > value) 
+        {
+                temp1=temp1->left;
+        } 
+        else
+        {
+                temp1=temp1->right;
+        }
+        }
+        return 0;
+    }
+
+    int lower_bound(int e)
+    {
+        Node* temp1;
+        temp1=b.head;
+        int diff=2147483647;
+        int closet=0;
+        if(!temp1)
+            return false;
+        while(temp1)
+        {                                   
+        if(temp1->val == e)
+        {
+              return temp1->val;         
+        }
+        else if(temp1->val > e) 
+        {
+            if(diff> abs(temp1->val - e) && temp1->val>=e)
+                {
+                    diff=abs(temp1->val - e);
+                    closet=temp1->val;
+                }
+                
+                temp1=temp1->left;
+            
+        } 
+        else
+        {
+            if(diff> abs(temp1->val - e) && temp1->val>=e)
+                {
+                    diff=abs(temp1->val - e);
+                    closet=temp1->val;
+                }
+            temp1=temp1->right;
+            
+        }
+        }
+        return closet;
+    }
+
+    int upper_bound(int e)
+    {
+        Node* temp1;
+        temp1=b.head;
+        int diff=2147483647;
+        int closet=0;
+        if(!temp1)
+            return false;
+        while(temp1)
+        {                                   
+        if(temp1->val == e)
+        {
+              if(temp1->right)
+                temp1=temp1->right;
+              else
+                return closet;         
+        }
+        else if(temp1->val > e) 
+        {
+            if(diff> abs(temp1->val - e) && temp1->val > e)
+                {
+                    diff=abs(temp1->val - e);
+                    closet=temp1->val;
+                }
+                
+                temp1=temp1->left;
+            
+        } 
+        else
+        {
+            if(diff> abs(temp1->val - e)  && temp1->val > e)
+                {
+                    diff=abs(temp1->val - e);
+                    closet=temp1->val;
+                }
+            temp1=temp1->right;
+            
+        }
+        }
+        return closet;
+    }
+
+
+    int Kth_largest(Node* headd,int k)
+    {
+     Node* init = headd;
+    Node* K = NULL;
+    int vaalue = 0;
+    while (init != NULL) {
+        if (init->right == NULL) {
+            vaalue++;
+            if (vaalue == k)
+                K = init;
+ 
+        
+            init = init->left;
+        }
+ 
+        else {
+            Node* lefty = init->right;
+            while (lefty->left != NULL && lefty->left != init)
+                lefty = lefty->left;
+ 
+            if (lefty->left == NULL) {
+                lefty->left = init;
+                init = init->right;
+            }
+            else {
+ 
+                lefty->left = NULL;
+                vaalue++;
+                if (vaalue == k)
+                    K = init;
+ 
+                init = init->left;
+            }
+        }
+    }
+    return K->val;
+}
+    
+    int count_range(int a,int bb)
+    {
+        Node* temp1;
+        temp1=b.head;
+        if(!temp1)
+            return 0;
+        while(temp1)
+        {                                   
+        if(temp1->val == a)
+        {
+               break;         
+        }
+        else if(temp1->val > a) 
+        {
+            if(temp1->left)
+            {
+                
+                temp1=temp1->left;
+            }
+            else
+            {
+                return 0;
+            }
+        } 
+        else
+        {
+            if(temp1->right)
+            {
+                temp1=temp1->right;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        }
+    
+        Node* temp=NULL;
+        if(temp1->right)
+            temp=temp1->right;
+        int left=total_node1(temp,a);
+        left++;
+        int cc=left;
+        while(temp1->prev != b.head)
+        {
+            temp1=temp1->prev;
+            Node* aaa=NULL;
+            if(temp1->right)
+                aaa=temp1->right;
+        
+            cc+=total_node1(aaa,a);
+            cc++;
+        }
+        cc++;
+    
+        left=cc;
+    
+        temp1=temp1->prev;
+        if(!temp1)
+            return 0;
+        while(temp1)
+        {                                   
+        if(temp1->val == bb)
+        {
+               break;         
+        }
+        else if(temp1->val > bb) 
+        {
+            if(temp1->left)
+            {
+                
+                temp1=temp1->left;
+            }
+            else
+            {
+                return 0;
+            }
+        } 
+        else
+        {
+            if(temp1->right)
+            {
+                temp1=temp1->right;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        }
+        Node* he=NULL;
+        if(temp1->left)
+            he=temp1->left;
+        int right=total_node2(he,bb);
+        int cb=right+1;
+        while(temp1->prev != b.head)
+        {
+            temp1=temp1->prev;
+            Node* aaa;
+            if(temp1->left)
+                aaa=temp1->left;
+            cb+=total_node2(aaa,bb);
+            cb++;
+        }
+    
+        right=cb;
+    
+        int total=left+right;
+        return total;
+    }
+
+     int total_node1(Node* root,int x){
+        if(root==NULL)
+            return 0;
+        
+        int leftNode=total_node1(root->left,x);
+        int rightNode=total_node1(root->right,x);
+        if (root->val>=x)
+            return 1+leftNode+rightNode;
+        int a=leftNode+rightNode;
+        return a;
+    }
+
+    int total_node2(Node* root,int x){
+        if(root==NULL)
+            return 0;
+        
+        int leftNode=total_node2(root->left,x);
+        int rightNode=total_node2(root->right,x);
+        if (root->val<x)
+            return 1+leftNode+rightNode;
+        int a=leftNode+rightNode;
+        return a;
+    }
+    
+    int abs(int a)
+    {
+        if(a<0)
+        return -a;
+        else
+        return a;
+    }
+
+    int closest_elements(int e)
+    {
+        Node* temp1;
+        temp1=b.head;
+        int diff=2147483647;
+        int closet;
+        if(!temp1)
+            return false;
+        while(temp1)
+        {                                   
+        if(temp1->val == e)
+        {
+              return temp1->val;         
+        }
+        else if(temp1->val > e) 
+        {
+            if(diff> abs(temp1->val - e))
+                {
+                    diff=abs(temp1->val - e);
+                    closet=temp1->val;
+                }
+                
+                temp1=temp1->left;
+            
+        } 
+        else
+        {
+            if(diff> abs(temp1->val - e))
+                {
+                    diff=abs(temp1->val - e);
+                    closet=temp1->val;
+                }
+            temp1=temp1->right;
+            
+        }
+        }
+        return closet;
+    }
 
     void preorder(Node* jj)
     {
@@ -1354,12 +1751,14 @@ class AVL
         if(jj->right)
             preorder(jj->right);
     }
-
+    
     void inorder(Node* jj)
     {
+    
         if(jj->left)
             inorder(jj->left);
         cout<<jj->val<<" ";
+    
         if(jj->right)
             inorder(jj->right);
         
@@ -1372,26 +1771,24 @@ int main()
 {
     AVL avl;
     avl.insert(1);
-    avl.insert(19);
-    avl.insert(10);
-    avl.insert(15);
-    avl.insert(23);
-    avl.insert(30);
-    avl.insert(28);
-    // avl.insert(5);
-    // avl.insert(25);
-    // avl.insert(19);
-    avl.dlt(19);
-    avl.dlt(1);
-    avl.dlt(10);
-    //cout<<endl;
-    //cout<<avl.b.head->left->right->val<<endl;
+    avl.insert(8);
+    avl.insert(13);
+    avl.insert(20);
+    avl.insert(16);
+    avl.insert(18);
+    avl.insert(25);
+    avl.insert(54);
+   avl.dlt(8);
+   bool bb=avl.search(5);
+   int cc=avl.count_occurence(3);
+   int ccc=avl.lower_bound(3);
+
     cout<<"inorder :"<<endl;
    avl.inorder(avl.b.head);
    cout<<endl;
    cout<<"preorder :"<<endl;
    avl.preorder(avl.b.head);
    cout<<endl;
-   cout<<avl.b.head->val<<endl;
+  
     return 0;
 }
